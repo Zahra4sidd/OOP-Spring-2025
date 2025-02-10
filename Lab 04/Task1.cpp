7f1c25a91f4b3056702f7d413937603e30dbce26
Zahra4sidd <siddiqui.zahra2006@gmail.com>
2025-02-10 21:07:32 +0500

    Create Task1.cpp

---
+++ Lab 04/Task1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Book
+{
+    private:
+    string title;
+    float price;
+    int *stock;
+    
+    public:
+    Book()
+    {
+        title = "Unknown";
+        price = 0.0;
+        stock = new int(30);
+    }
+    ~Book()
+    {
+        delete stock;
+    }
+    void updateDetails(string t,float p)
+    {
+        title = t;
+        price = p;
+    }
+    void purchaseBook()
+    {
+        string name;
+        cout<<"Enter the book title to purchase: ";
+        getline(cin,name);
+        int quantity;
+        if(name!=title)
+        {
+            cout<<"Book not found!\n";
+            return;
+        }
+        cout<<"Book found!\n";
+        cout<<"Enter quantity of books you want to purchase: ";
+        cin>>quantity;
+        if(quantity>(*stock))
+        {
+            cout<<"Not enough stock\n";
+            return;
+        }
+        (*stock) -= quantity;
+        cout<<quantity<<" books successfully purchased\n";
+        cout<<"Total price: "<<price*quantity<<endl;
+        cout<<"Remaining stock: "<<*stock<<endl;
+        if(*stock < 5)
+        {
+            cout<<"Warning!! Low stock\n";
+        }
+    }
+};
+
+int main()
+{
+    Book b1;
+    b1.updateDetails("Silent Patient",500.0);
+    b1.purchaseBook();
+}
