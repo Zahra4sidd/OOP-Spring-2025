f9a5c3f786870062f13d9177c29f1043808b1c77
Zahra4sidd <siddiqui.zahra2006@gmail.com>
2025-04-18 19:00:12 +0500

    Update Task3.cpp

--- Lab 09/Task3.cpp
+++ Lab 09/Task3.cpp
@@ -1,3 +1,38 @@
+//Book.h
+#ifndef BOOK
+#define BOOK
+
+#include"iostream"
+using namespace std;
+
+#define MAX_BOOK 5
+
+class Book{
+   string title, author, isbn;
+
+public:
+   Book(string t, string a, string isbn);
+   Book() {}
+
+   string getTitle() const;
+   string getAuthor() const;
+   string getISBN() const;
+};
+
+class Library{
+   Book book[MAX_BOOK];
+   int bookCount = 0;
+
+public:
+   Library() {}
+   void AddBooks(Book b);
+   void Remove(string isbn);
+   void Search(string isbn, string title);
+};
+
+#endif
+
+//Book.cpp
 #include"iostream"
 #include"Book.h"
 using namespace std;
