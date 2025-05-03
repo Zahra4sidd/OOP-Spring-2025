da89277d6d6c2118ef50f3627be9022bf3d38b6a
Zahra4sidd <siddiqui.zahra2006@gmail.com>
2025-05-03 18:28:35 +0500

    Create Task9.cpp

---
+++ Lab 10/Task9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<cstring>
+#include <sstream>
+using namespace std;
+
+int main(){
+    ifstream infile("large_log.txt");
+    if(!infile){
+        cerr<<"Error opening file\n";
+        exit(0);
+    }
+    infile.seekg(ios::beg);
+    char buffer[11] = {0};
+    infile.read(buffer,10);
+    cout<<buffer<<endl;
+    streampos pos1 = infile.tellg();
+    cout<<"Position after first read:"<<pos1<<endl;
+    infile.read(buffer,10);
+    cout<<buffer<<endl;
+    streampos pos2 = infile.tellg();
+    cout<<"Position after second read:"<<pos2<<endl;
+    infile.close();
+    return 0;
+}
